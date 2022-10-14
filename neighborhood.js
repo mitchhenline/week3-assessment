d3e20f82d33d461425830f2e3ef8ea9012993924
Mitch Henline <rmhenline@gmail.com>
2022-10-14 11:55:38 -0600

    all done

---
+++ neighborhood.js
@@ -0,0 +1,14 @@
+let restaurantRec = document.querySelector('#restaurant')
+restaurantRec.addEventListener('click', giveAPlaceToEat)
+
+
+let restaurantText = "#restaurant-text"
+
+
+const restArr = ['Bombay House', 'McDonalds', 'That one taco cart on 600 South', 'MoBettas', 'The Pie',]
+
+
+// function giveAPlaceToEat(){
+//     const random = Math.floor(Math.random() * restArr.length);
+//     restaurantText.textContent = restArr(random)
+// }
